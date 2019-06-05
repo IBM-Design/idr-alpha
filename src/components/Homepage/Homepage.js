e0a19af18f287d103c36e0aa36fa07ef998243c5
Scott <sstrubberg@us.ibm.com>
2019-06-05 16:20:37 -0500

    updated the hero with new copy and new svg

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -2,122 +2,6 @@ import React from 'react';
 import ArrowRight20 from '@carbon/icons-react/es/arrow--right/20';
 import Link from 'react';
 
-const doubleCircle = (
-  <svg
-    width="71"
-    height="44"
-    viewBox="0 0 71 44"
-    xmlns="http://www.w3.org/2000/svg">
-    <g id="IDR" fill="none" fillRule="evenodd">
-      <g id="1" transform="translate(-354 -1283)" stroke="#F3F3F3">
-        <g id="block-asset-copy-2" transform="translate(339 1082)">
-          <g id="Screen-Shot-2018-03-04-at-10.50.44-PM">
-            <g id="Group" transform="translate(15 201)">
-              <circle id="Oval" cx="22" cy="22" r="21.5" />
-              <circle id="Oval-Copy" cx="49" cy="22" r="21.5" />
-            </g>
-          </g>
-        </g>
-      </g>
-    </g>
-  </svg>
-);
-
-const shapesOnLine = (
-  <svg
-    width="55"
-    height="43"
-    viewBox="0 0 55 43"
-    xmlns="http://www.w3.org/2000/svg">
-    <g id="IDR" fill="none" fillRule="evenodd" strokeLinejoin="round">
-      <g
-        id="1"
-        transform="translate(-675 -1287)"
-        stroke="#F3F3F3"
-        strokeWidth="1.11">
-        <g id="block-asset-copy-3" transform="translate(658 1082)">
-          <g id="Screen-Shot-2018-03-04-at-10.50.44-PM">
-            <g id="Group-20" transform="rotate(-180 35.5 123.5)">
-              <path
-                d="M46.3877551,0 L46.3877551,0 C42.7359131,6.70832496e-16 39.7755102,2.96040288 39.7755102,6.6122449 L39.7755102,6.6850524 C39.7755102,10.3368944 42.7359131,13.2972973 46.3877551,13.2972973 L46.3877551,13.2972973 C50.0395971,13.2972973 53,10.3368944 53,6.6850524 L53,6.6122449 C53,2.96040288 50.0395971,-6.70832496e-16 46.3877551,0 Z"
-                id="Stroke-3"
-              />
-              <polygon
-                id="Stroke-4"
-                points="52.8908378 27.2679068 39.6663481 27.2679068 39.6663481 40.5652041 52.8908378 40.5652041"
-              />
-              <polyline
-                id="Stroke-5"
-                points="11.5624193 20.271082 25.6135187 20.271082 25.6135187 33.9700267 39.6673098 33.9700267"
-              />
-              <polygon
-                id="Stroke-3-Copy"
-                points="11.7668442 13 0 20.6727494 11.7668442 27.6285315"
-              />
-              <polyline
-                id="Stroke-6"
-                points="39.6661076 6.65982404 25.5738153 6.65982404 25.5738153 21.25"
-              />
-            </g>
-          </g>
-        </g>
-      </g>
-    </g>
-  </svg>
-);
-
-const shapesAroundPerson = (
-  <svg
-    width="43"
-    height="44"
-    viewBox="0 0 43 44"
-    xmlns="http://www.w3.org/2000/svg">
-    <g
-      id="IDR"
-      fill="none"
-      fillRule="evenodd"
-      strokeLinecap="round"
-      strokeLinejoin="round">
-      <g
-        id="1"
-        transform="translate(-995 -1285)"
-        stroke="#F3F3F3"
-        strokeWidth="0.866">
-        <g id="block-asset-copy-4" transform="translate(977 1082)">
-          <g id="Screen-Shot-2018-03-04-at-10.50.44-PM">
-            <g id="Group-13" transform="translate(18 204)">
-              <path
-                d="M33.8080471,42.8981882 C33.8080471,36.8844235 29.6345176,31.8477176 24.0271059,30.5186588 C26.5648706,29.3888941 28.3365176,26.8498353 28.3365176,23.8914824 C28.3365176,19.8861882 25.0895765,16.6379529 21.0829882,16.6379529 C17.0764,16.6379529 13.8281647,19.8861882 13.8281647,23.8914824 C13.8281647,26.8498353 15.5998118,29.3888941 18.1375765,30.5186588 C12.5301647,31.8477176 8.35663529,36.8844235 8.35663529,42.8981882"
-                id="Stroke-1"
-              />
-              <path
-                d="M15.0409896,6.055365 C14.3502066,5.36621495 14.0038584,4.46516559 14.0000314,3.56220722 C13.9962043,2.64970384 14.3425525,1.73529145 15.0409896,1.0385054 C15.733686,0.34744635 16.6406974,0.00191682675 17.5496222,7.82386022e-06 C18.4604607,-0.00190117903 19.3732126,0.345537347 20.0697361,1.0385054 L20.9997098,1.96818981 L21.931597,1.0385054 C22.6242934,0.345537347 23.5351318,7.82386022e-06 24.4459702,7.82386022e-06 C25.3548951,7.82386022e-06 26.2657335,0.345537347 26.9603435,1.0385054 C27.6702617,1.74674547 28.0166099,2.68215689 27.9993882,3.60993229 C27.98408,4.49761864 27.6377317,5.37957797 26.9603435,6.055365 L20.9997098,12 L15.0409896,6.055365 Z"
-                id="Stroke-3"
-              />
-              <path
-                d="M41.6043294,27.5023647 C41.6043294,25.2557765 39.7835059,23.4349529 37.5356235,23.4349529 C35.2903294,23.4349529 33.4682118,25.2557765 33.4682118,27.5023647 C33.4682118,29.7489529 35.2903294,31.5697765 37.5356235,31.5697765 C39.7835059,31.5697765 41.6043294,29.7489529 41.6043294,27.5023647 Z"
-                id="Stroke-5"
-              />
-              <polygon
-                id="Stroke-9"
-                points="2.28023529 17.1188824 10.4150588 17.1188824 10.4150588 8.98405882 2.28023529 8.98405882"
-              />
-              <polygon
-                id="Stroke-11"
-                points="31.9187647 11.4468941 35.8722941 9.16407059 39.8258235 11.4468941 39.8258235 16.0125412 35.8722941 18.2940706 31.9187647 16.0125412"
-              />
-              <polygon
-                id="Stroke-12"
-                points="4.62789412 22.9373647 0.560482353 31.0721882 8.69530588 31.0721882"
-              />
-            </g>
-          </g>
-        </g>
-      </g>
-    </g>
-  </svg>
-);
-
 export class HomepageHeader extends React.Component {
   componentDidMount() {
     document.title = 'IBM Design Research';
@@ -127,153 +11,101 @@ export class HomepageHeader extends React.Component {
     // Banner image
     const bannerImg = (
       <svg
-        width="304"
-        height="304"
-        viewBox="0 0 304 304"
-        xmlns="http://www.w3.org/2000/svg">
-        <g id="Circle-matrix" fill="none" fillRule="evenodd">
-          <g id="Group-3">
-            <circle
-              id="Oval-Copy-15"
-              stroke="#DCDCDC"
-              strokeWidth="0.8"
-              cx="192"
-              cy="112"
-              r="32"
+        width="994"
+        height="986"
+        viewBox="0 0 994 986"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink">
+        <defs>
+          <ellipse id="path-1" cx="496" cy="492" rx="496" ry="492" />
+          <mask id="mask-2" x="0" y="0" width="992" height="984" fill="#fff">
+            <use xlinkHref="#path-1" />
+          </mask>
+        </defs>
+        <g id="Pulse" fill="none" fillRule="evenodd">
+          <g id="Group" transform="translate(1 1)">
+            <ellipse
+              id="Oval-10"
+              fill="#FFA0C2"
+              opacity="0.2"
+              cx="493.5"
+              cy="493"
+              rx="267.5"
+              ry="265"
             />
-            <circle
-              id="Oval-Copy-25"
-              stroke="#DCDCDC"
-              strokeWidth="0.8"
-              cx="192"
-              cy="32"
-              r="32"
+            <path
+              d="M494.5,788 C658.804713,788 792,655.700144 792,492.5 C792,329.299856 658.804713,197 494.5,197 C330.195287,197 197,329.299856 197,492.5 C197,655.700144 330.195287,788 494.5,788 Z"
+              id="Oval-10"
+              stroke="#FF767C"
+              strokeWidth="2"
+              strokeDasharray="2,10"
             />
-            <circle
-              id="Oval-Copy-26"
-              stroke="#DCDCDC"
-              strokeWidth="0.8"
-              cx="272"
-              cy="32"
-              r="32"
+            <ellipse
+              id="Oval-10"
+              fill="#FA75A6"
+              opacity="0.5"
+              cx="494.5"
+              cy="494"
+              rx="213.5"
+              ry="212"
             />
-            <circle
-              id="Oval-Copy-18"
-              stroke="#DCDCDC"
-              strokeWidth="0.8"
-              cx="192"
-              cy="272"
-              r="32"
+            <ellipse
+              id="Oval-10"
+              fill="#EE538B"
+              cx="493.5"
+              cy="495"
+              rx="150.5"
+              ry="150"
             />
-            <circle
-              id="Oval-Copy-12"
-              stroke="#DCDCDC"
-              strokeWidth="0.8"
-              cx="32"
-              cy="192"
-              r="32"
+            <path
+              d="M495,866 C703.211351,866 872,698.778354 872,492.5 C872,286.221646 703.211351,119 495,119 C286.788649,119 118,286.221646 118,492.5 C118,698.778354 286.788649,866 495,866 Z"
+              id="Oval-10"
+              stroke="#EE538B"
+              strokeWidth="2"
+              strokeDasharray="2,10"
             />
-            <circle
-              id="Oval-Copy-16"
-              stroke="#DCDCDC"
-              strokeWidth="0.8"
-              cx="112"
-              cy="112"
-              r="32"
+            <use
+              id="Oval-10"
+              stroke="#EE538B"
+              mask="url(#mask-2)"
+              strokeWidth="4"
+              opacity="0.6"
+              strokeDasharray="2,10"
+              xlinkHref="#path-1"
             />
-            <circle
-              id="Oval-Copy-27"
-              stroke="#DCDCDC"
-              strokeWidth="0.8"
-              cx="32"
-              cy="112"
-              r="32"
+            <path
+              d="M494.5,846 C691.389513,846 851,687.956517 851,493 C851,298.043483 691.389513,140 494.5,140 C297.610487,140 138,298.043483 138,493 C138,687.956517 297.610487,846 494.5,846 Z"
+              id="Oval-10-Copy"
+              stroke="#EE538B"
+              strokeWidth="2"
+              opacity="0.2"
+              strokeDasharray="2,10"
             />
-            <circle
-              id="Oval-Copy-21"
-              stroke="#DCDCDC"
-              strokeWidth="0.8"
-              cx="112"
-              cy="32"
-              r="32"
-            />
-            <circle
-              id="Oval-Copy-28"
-              stroke="#DCDCDC"
-              strokeWidth="0.8"
-              cx="32"
-              cy="32"
-              r="32"
-            />
-            <circle
-              id="Oval-Copy-19"
-              stroke="#DCDCDC"
-              strokeWidth="0.8"
-              cx="112"
-              cy="272"
-              r="32"
-            />
-            <circle
-              id="Oval-Copy-29"
-              stroke="#DCDCDC"
-              strokeWidth="0.8"
-              cx="32"
-              cy="272"
-              r="32"
-            />
-            <circle
-              id="Oval-Copy-11"
-              stroke="#DCDCDC"
-              strokeWidth="0.8"
-              cx="272"
-              cy="189"
-              r="32"
-            />
-            <circle
-              id="Oval-Copy-17"
-              stroke="#DCDCDC"
-              strokeWidth="0.8"
-              cx="272"
-              cy="112"
-              r="32"
-            />
-            <circle
-              id="Oval-Copy-20"
-              stroke="#DCDCDC"
-              strokeWidth="0.8"
-              cx="272"
-              cy="272"
-              r="32"
-            />
-            <circle
-              id="Oval"
-              stroke="#DCDCDC"
-              strokeWidth="0.8"
-              cx="192"
-              cy="192"
-              r="32"
-            />
-            <circle id="Oval-Copy-10" fill="#EE538B" cx="112" cy="192" r="32" />
           </g>
         </g>
       </svg>
     );
 
     const homeTitle = (
-      <h3 className="homepage--header__title">
-        <span>We are not our users.</span>
-      </h3>
+      <div className="homepage--header__wrapper">
+        <h3 className="homepage--header__title">
+          <span>We are not our users.</span>
+        </h3>
+        <p className="homepage--header__subtitle">
+          An authentic focus on people begins with this recognition.
+        </p>
+      </div>
     );
 
     return (
       <div>
         <section className="homepage--header">
           <div className="ibm--grid">
-            <div className="ibm--row">
-              <div className="ibm--col-sm-4 ibm--col-md-4 ibm--col-lg-4 ibm--offset-lg-4">
+            <div className="ibm--row ">
+              <div className="ibm--col-sm-4 ibm--col-md-4 ibm--col-lg-5 ibm--offset-lg-4">
                 {homeTitle}
               </div>
-              <div className="banner-svg ibm--col-sm-4 ibm--col-md-2 ibm-offset-md-2 ibm--col-lg-4 ibm--offset-lg-3">
+              <div className="banner-svg ibm--col-sm-4 ibm--col-md-2 ibm--col-lg-4 ibm--offset-lg-1">
                 {bannerImg}
               </div>
             </div>
@@ -285,7 +117,7 @@ export class HomepageHeader extends React.Component {
               <aside
                 aria-label="header callout"
                 className="ibm--col-md-8 ibm--col-lg-4 ibm--offset-lg-4">
-                <h3 className="bx--type-heading-02">
+                <h3>
                   What is IBM Design
                   <br />
                   Research?
@@ -329,7 +161,6 @@ export class HomepageTileNav extends React.Component {
                 <p className="bx--type-productive-heading-05">
                   A foundation for great experiences
                 </p>
-                <div>{doubleCircle}</div>
                 <div className="homepage-nav-tile-wrapper">
                   Read more <ArrowRight20 aria-label="read more" />
                 </div>
@@ -345,7 +176,7 @@ export class HomepageTileNav extends React.Component {
                 <p className="bx--type-productive-heading-05">
                   Establish a shared vocabulary
                 </p>
-                <div>{shapesOnLine}</div>
+
                 <div className="homepage-nav-tile-wrapper">
                   Read more <ArrowRight20 aria-label="read more" />
                 </div>
@@ -360,7 +191,7 @@ export class HomepageTileNav extends React.Component {
                 <p className="bx--type-productive-heading-05">
                   The design research code of conduct
                 </p>
-                <div>{shapesAroundPerson}</div>
+
                 <div className="homepage-nav-tile-wrapper">
                   Read more <ArrowRight20 aria-label="read more" />
                 </div>
